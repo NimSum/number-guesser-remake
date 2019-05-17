e62163f3ade341095774105ed112a3bd9bb25baf
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-16 21:42:08 -0600

    Append winner card to right side

--- src/js/domUpdates.js
+++ src/js/domUpdates.js
@@ -8,3 +8,26 @@ export const updateLatestScore = (idx, name, guess, message) => {
   document.querySelector(`.player-${idx + 1}-guess`).textContent = guess;
   document.querySelector(`.player-${idx + 1}-feedback`).textContent = message;
 }
+
+export const addWinnerCard = (p1, p2, winner, count, time) => {
+  document.querySelector('.winner-card-container').insertAdjacentHTML('afterbegin', `
+    <article class="winner-card">
+      <h4>
+        <span class="card-p1-name">${p1}</span>
+        vs
+        <span class="card-p2-name">${p2}</span>
+      </h4>
+      <h3 class="winner-name">${winner}</h3>
+      <h3>WINNER</h3>
+      <div>
+        <p><span class="guess-count">${count}</span> Guesses</p>
+        <p><span class="guess-time">${time}</span> Mins</p>
+        <button class="delete-card">X</button>
+      </div>
+    </article>
+  `)
+}
+
+export const deleteCard = (e) => {
+  e.target.parentElement.parentElement.remove();
+}
--- src/js/game.js
+++ src/js/game.js
@@ -4,21 +4,40 @@ import Player from './Player';
 export const startGame = () => {
   let minRange = 1;
   let maxRange = 100;
+  let randomNum = generateRandomNum();
+  let guessCount = 0;
+  let timer = 1;
+  const getValue = (className) => document.querySelector(className).value;
   const playerOne = new Player()
   const playerTwo = new Player()
   document.querySelector('.submit-guesses-btn')
-    .addEventListener('click', handleSubmit)
+    .addEventListener('click', handleSubmit);
+  document.querySelector('.submit-new-range')
+    .addEventListener('click', updateNumberRange);
+  document.querySelector('.winner-card-container')
+    .addEventListener('click', deleteCard);
+
+  function deleteCard(e) {
+    e.target.className === 'delete-card' && domUpdate.deleteCard(e)
+  }
+
+  function updateNumberRange(e) {
+    e.preventDefault();
+    minRange = parseInt(getValue('.new-min'));
+    maxRange = parseInt(getValue('.new-max'));
+    domUpdate.updateNumberRange(minRange, maxRange);
+    randomNum = generateRandomNum()
+  }
 
   function handleSubmit() {
-    const getValue = (className) => document.querySelector(className).value;
     playerOne.name = getValue('.player-1-name-input');
     playerTwo.name = getValue('.player-2-name-input');
     playerOne.guess = getValue('.player-1-guess-input');
     playerTwo.guess = getValue('.player-2-guess-input');
+    guessCount++;
     checkPlayerGuesses();
   }
-  const randomNum = generateRandomNum();
-  console.log(randomNum)
+  
   function checkPlayerGuesses() {
     [ playerOne, playerTwo ].forEach((player, idx) => {
       const { name, guess } = player;
@@ -31,13 +50,27 @@ export const startGame = () => {
         break;
       default: 
         domUpdate.updateLatestScore(idx, name, guess, 'BOOM!');
+        domUpdate.addWinnerCard(playerOne.name, playerTwo.name, player.name, guessCount, getTime());
+        increaseNumRange();
       }
     })
+  }
+
+  const getTime = () => (timer / 60).toFixed(2);
+
+  function increaseNumRange() {
+    minRange < 11 ? minRange = 1 : minRange -= 10;
+    maxRange += 10;
+    randomNum = generateRandomNum();
+    domUpdate.updateNumberRange(minRange, maxRange);
+    guessCount = 0;
+    timer = 1;
   }
 
   function generateRandomNum() {
     return Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
   }
 
-
+  const timerInterval = () => setInterval(timer++, 1000);
+  timerInterval()
 }
